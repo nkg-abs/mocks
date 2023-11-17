aa28dcbbeb4a55cde0ace07be1ae9558207769d1
nkg-abs <nkg@abs.in>
2023-11-17 11:46:27 +0530

    Refactored.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const fakerParser = require("faker-parser");
 
-const parser = fakerParser({})
+const parser = fakerParser({});
 const taskTree = () => {
     return parser("name.firstName")
 };
 
 module.exports = {
     taskTree
-}
+};
